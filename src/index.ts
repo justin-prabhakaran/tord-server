85b90f6fd4be38baa5248e3e3c85284129fb93c2
justinprabhakaran <c.m.justinprabhakaran@gmail.com>
2024-03-15 00:13:21 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,77 @@
 import express, { Request, Response } from "express";
 import { truth, dare } from "./data/data";
 
-async function getTruth(): Promise<string> {
-    return new Promise((resolve) => {
-        let r = Math.ceil(Math.random() * truth.length);
-        if (truth[r]) {
-            resolve(truth[r]);
-        } else {
-            resolve(getTruth());
-        }
-    });
-}
-
-async function getDare(): Promise<string> {
-    return new Promise((resolve) => {
-        let r = Math.ceil(Math.random() * dare.length);
-        if (dare[r]) {
-            resolve(dare[r]);
-        } else {
-            resolve(getTruth());
-        }
-    });
-}
-
 const app = express();
 
 app.use(express.json());
 
+async function getTruth(): Promise<string> {
+  return new Promise((resolve) => {
+    let r = Math.ceil(Math.random() * truth.length);
+    if (truth[r]) {
+      resolve(truth[r]);
+    } else {
+      resolve(getTruth());
+    }
+  });
+}
+
+async function getDare(): Promise<string> {
+  return new Promise((resolve) => {
+    let r = Math.ceil(Math.random() * dare.length);
+    if (dare[r]) {
+      resolve(dare[r]);
+    } else {
+      resolve(getDare());
+    }
+  });
+}
 
 app.get("/", (req: Request, res: Response) => {
-    console.log(req.method + " Connection Called");
-    res.status(200).send({
-        status: "ok",
-        message: "connected",
-        data: req.params,
-    });
+  console.log(req.method + " Connection Called");
+  res.status(200).send({
+    status: "ok",
+    message: "connected",
+    data: req.params,
+  });
 });
 
 app.get("/truth", (req: Request, res: Response) => {
-    console.log(req.method + " Truth called");
-    getTruth()
-        .then((data) => {
-            let js = {
-                status: "ok",
-                truth: data,
-            };
-            res.status(200).send(js);
-        })
-        .catch(() => {
-            res.status(500).send({
-                status: "fucked",
-                reson: "server error",
-            });
-        });
-
+  console.log(req.method + " Truth called");
+  getTruth()
+    .then((data) => {
+      let js = {
+        status: "ok",
+        truth: data,
+      };
+      res.status(200).send(js);
+    })
+    .catch(() => {
+      res.status(500).send({
+        status: "fucked",
+        reason: "server error",
+      });
+    });
 });
 
 app.get("/dare", (req: Request, res: Response) => {
-    console.log(req.method + " Dare called");
-    getDare()
-        .then((data) => {
-            let js = {
-                status: "ok",
-                truth: data,
-            };
-            res.status(200).send(js);
-        })
-        .catch(() => {
-            res.status(500).send({
-                status: "fucked",
-                reson: "server error",
-            });
-        });
+  console.log(req.method + " Dare called");
+  getDare()
+    .then((data) => {
+      let js = {
+        status: "ok",
+        dare: data,
+      };
+      res.status(200).send(js);
+    })
+    .catch(() => {
+      res.status(500).send({
+        status: "fucked",
+        reason: "server error",
+      });
+    });
 });
 
-
-
 app.listen(3000, () => {
-    console.log("SERVER IS RUNNING");
+  console.log("SERVER IS RUNNING");
 });
